4809c5e92b351a638dede3d179c43b98e2bc2970
Praveen <praveen.chauhan915@gmail.com>
2025-04-12 18:46:05 +0530

    String Coding

--- src/main/java/PractiseCoding/hashSetExample.java
+++ src/main/java/PractiseCoding/hashSetExample.java
@@ -1,4 +1,32 @@
 package PractiseCoding;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class hashSetExample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+        set.add("Four");
+        set.add("Five");
+        set.add("Six");
+        set.add("Seven");
+        set.add("Eight");
+        set.add("Nine");
+        set.add("Ten");
+        System.out.println(set);
+        System.out.println("Size of HashSet:- " +set.size());
+        System.out.println("Contains Five:- " +set.contains("Five"));
+        System.out.println("**************************************");
+        //Using Iterator
+        Iterator itr = set.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+        set.add("Ten");
+        System.out.println("HashSet after adding Ten:- " +set);
+    }
 }
--- src/main/java/PractiseCoding/regularExpression.java
+++ src/main/java/PractiseCoding/regularExpression.java
@@ -1,4 +1,28 @@
 package PractiseCoding;
 
+import java.util.regex.Pattern;
+
 public class regularExpression {
+    public static void main(String[] args) {
+        // Data format (dd/mm/yyyy)
+        String dateregx = "^(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)$";
+//                         Explanation: ^(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\d\d)$
+//                                      ^ and $ = start and end of the string.
+//                                      (0?[1-9]|[12][0-9]|3[01]) = matches 01 to 31
+//                                       / = slash separator
+//                                       (0?[1-9]|1[012]) =  matches 01 to 12
+//                                       (19|20)\\d\\d = matches years starting with 19 and 20
+        System.out.println("Date '25/12/2023' is valid: " + Pattern.matches(dateregx, "25/12/2023"));
+        System.out.println("Date '32/01/2024' is valid: " + Pattern.matches(dateregx, "32/01/2024"));
+
+        // Email format
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+//         Explanation Email:- ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$
+//                             ^[a-zA-Z0-9._%+-]+ = username part (letters, numbers, dots, etc.)
+//                             @ = at symbol
+//                             [a-zA-Z0-9.-]+ = domain name (e.g., gmail, yahoo)
+//                             \\.[a-zA-Z]{2,}$ = dot followed by 2 or more letters (like .com, .org)
+        System.out.println("Email 'test@example.com' is valid: " + Pattern.matches(emailRegex, "test@example.com"));
+        System.out.println("Email 'invalid.email' is valid: " + Pattern.matches(emailRegex, "invalid.email"));
+    }
 }
